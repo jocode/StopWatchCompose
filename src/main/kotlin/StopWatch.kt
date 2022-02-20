6dbc2c0b79cb84da4d6a316d9f9d2e5357d591f5
Johan Mosquera <joncamilomoni@gmail.com>
2022-02-19 20:02:36 -0500

    Jetpack Compose on Desktop

---
+++ src/main/kotlin/StopWatch.kt
@@ -0,0 +1,61 @@
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import kotlinx.coroutines.*
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+class StopWatch {
+
+    var formattedTime by mutableStateOf("00:00:00")
+
+    private var coroutineScope = CoroutineScope(Dispatchers.Main)
+    private var isActive = false
+
+    private var timeInMillis = 0L
+    private var lastTimeStamp = 0L
+
+
+    fun start() {
+        if (isActive) return
+
+        coroutineScope.launch {
+            lastTimeStamp = System.currentTimeMillis()
+            this@StopWatch.isActive = true
+
+            while (this@StopWatch.isActive) {
+                delay(10L)
+                timeInMillis += System.currentTimeMillis() - lastTimeStamp
+                lastTimeStamp = System.currentTimeMillis()
+                formattedTime = formatTime(timeInMillis)
+            }
+        }
+    }
+
+    fun pause() {
+        isActive = false
+    }
+
+    fun reset() {
+        coroutineScope.cancel()
+        coroutineScope = CoroutineScope(Dispatchers.Main)
+        timeInMillis = 0L
+        lastTimeStamp = 0L
+        formattedTime = "00:00:00"
+        isActive = false
+    }
+
+    private fun formatTime(timeMillis: Long): String {
+        val localDateTime = LocalDateTime.ofInstant(
+            Instant.ofEpochMilli(timeMillis),
+            ZoneId.systemDefault()
+        )
+        val formatter = DateTimeFormatter.ofPattern("mm:ss:SSS", Locale.getDefault())
+
+        return localDateTime.format(formatter)
+    }
+
+}
